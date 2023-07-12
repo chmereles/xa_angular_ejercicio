9dd562c6faaa6cf545709c0fa6af844be19e50e0
Chr Mereles <chmereles@gmail.com>
2023-07-12 17:00:16 -0300

    Updated README

--- src/app/core/services/product/product-service.service.ts
+++ src/app/core/services/product/product-service.service.ts
@@ -5,16 +5,16 @@ import { Product } from '../../Interfaces/product';
   providedIn: 'root'
 })
 export class ProductService {
-  private _products: Product[] = [];
+  private products: Product[] = [];
 
   constructor() {
-    this._products.push({ name: 'Coca Cola', cost: '1275,00' })
-    this._products.push({ name: 'Pepsi', cost: '960,00' })
-    this._products.push({ name: 'Fanta', cost: '1180,00' })
-    this._products.push({ name: 'Sprite', cost: '950,00' })
+    this.products.push({ name: 'Coca Cola', cost: '1275,00' })
+    this.products.push({ name: 'Pepsi', cost: '960,00' })
+    this.products.push({ name: 'Fanta', cost: '1180,00' })
+    this.products.push({ name: 'Sprite', cost: '950,00' })
   }
 
   getProducts(): Product[] {
-    return this._products;
+    return this.products;
   }
 }
